0c4f207c23b35e3f3b03a1a8f377a5b08a626b56
ojaswi-454 <aggojaswi@gmail.com>
2021-06-27 17:44:26 +0530

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+import React from "react";
+
+function Footer(){
+
+
+    return (
+    <footer><p>
+        Copyright © {new Date().getFullYear()}
+    </p>
+    </footer>
+    );
+}
+export default Footer;
